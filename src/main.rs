b0c61e3bc5118e07c1ee047b0f7293ec9d59d792
Pablo Almeida <estebanpablo89@email.com>
2025-01-15 20:33:19 -0500

    borrowing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+
+#[derive(Debug)]
+struct Account {
+    id: u32,
+    balance: i32,
+    holder: String,
+}
+
+
+impl Account {
+    fn new(id: u32, holder: String) -> Self {
+        Account { 
+            id: id,
+            holder: holder,
+            balance: 0,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Bank {
+    accounts: Vec<Account>,
+}
+
+impl Bank {
+    fn new() -> Self {
+        Bank { accounts: vec![]}
+    }
+}
+
+fn print_account(account: Account) {
+    println!("Account: {:#?}", account);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let bank = Bank::new();
+
+    let other_bank = bank;
+
+    print!("{:#?}", bank);
 }
